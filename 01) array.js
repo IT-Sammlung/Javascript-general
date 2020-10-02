78042f70e07a73d94d19160ca7564f0c7dc920f5
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-02 18:13:42 +0200

    Update 01) array.js

--- 01) array.js
+++ 01) array.js
@@ -1,4 +1,7 @@
-// Verkettung von Array-Methoden wie map, filter oder reduce
+// Verkettung von Array-Methoden wie map,
 const array1 = ['1', 'Max', '2', '3', '4', '5', 'Peter', '6', '7', '8', '9'];
-const result = array1.map(x => parseInt(x)).filter(x => !isNaN(x)).reduce((x, y) => x + y);
+const result = array1
+.map(x => parseInt(x)) // ['1', 'NaN', '2', '3', '4', '5', 'NaN', '6', '7', '8', '9'];
+.filter(x => !isNaN(x)) // ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+.reduce((x, y) => x + y); // 1+2+3+4+5+6+7+8+9 = 45
 console.log(result); // 45
