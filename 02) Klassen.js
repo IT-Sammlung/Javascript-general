1576bef818fd83a39cdd9c5ae4341f2b5efe82e7
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-05 18:20:35 +0200

    Create 02) Klassen.js

---
+++ 02) Klassen.js
@@ -0,0 +1,17 @@
+// Einfaches Basis-Beispiel
+// Objekterzeugung durch Klassen-Syntax [ES05]
+class Tier {
+  constructor(name, typ, alter) {
+    this.name = name;
+    this.typ = typ;
+    this.alter = alter;
+  }
+  // Methode
+  log() {
+    console.log(this.name,this.typ,this.alter);
+  }
+}
+const tigerchen = new Tier('Tigerchen', "Katze", 3);
+tigerchen.log(); // "Tigerchen", "Katze", 3
+
+
