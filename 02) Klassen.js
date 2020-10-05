ff6024e10e7c052fb90cb4e199bd36d013ececba
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-05 18:46:01 +0200

    Update 02) Klassen.js

--- 02) Klassen.js
+++ 02) Klassen.js
@@ -15,3 +15,16 @@ const tigerchen = new Tier('Tigerchen', "Katze", 3);
 tigerchen.log(); // "Tigerchen", "Katze", 3
 
 
+// Prototypische Vererbung
+class Tier {
+  constructor(name, typ, alter) {
+    this.name = name;
+    this.typ = typ;
+    this.alter = alter;
+  }
+}
+Tier.prototype.log = function() {
+  console.log(this.name,this.typ,this.alter);
+}
+const tigerchen = new Tier('Tigerchen', "Katze", 3);
+tigerchen.log(); // "Tigerchen", "Katze", 3
