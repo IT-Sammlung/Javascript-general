6bc92e78f546699bb079d2eea3fc9a4ebc2b30f1
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-05 19:13:47 +0200

    Update 02) Klassen.js

--- 02) Klassen.js
+++ 02) Klassen.js
@@ -15,16 +15,27 @@ const tigerchen = new Tier('Tigerchen', "Katze", 3);
 tigerchen.log(); // "Tigerchen", "Katze", 3
 
 
+
+
 // Pseudoklassische Vererbung
-class Tier {
-  constructor(name, typ, alter) {
+// Definiere Klasse Tier
+function Tier(name, alter) {
     this.name = name;
-    this.typ = typ;
     this.alter = alter;
-  }
 }
+// Definiere unterklasse von Tier
+function Katze(name, farbe, alter) {
+  Tier.call(this, name, alter);
+  this.typ = 'Katze';
+  this.farbe = farbe;
+}
+// Erweitere Tier Klasse
 Tier.prototype.log = function() {
-  console.log(this.name,this.typ,this.alter);
+  console.log(this.name,this.typ,this.farbe,this.alter);
 }
-const tigerchen = new Tier('Tigerchen', "Katze", 3);
-tigerchen.log(); // "Tigerchen", "Katze", 3
+// Bindung
+Katze.prototype = new Tier();
+Katze.prototype.constructor = Katze;
+// Aufruf
+const tigerchen = new Katze('Tigerchen', 3, "Schwarz");
+tigerchen.log(); // "Tigerchen", "Katze", 3, "Schwarz";
