d8a6ab495584efaad3f30e725e578a6836022545
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-05 22:34:17 +0200

    Update 03) Datenkapselung.js

--- 03) Datenkapselung.js
+++ 03) Datenkapselung.js
@@ -13,6 +13,7 @@ console.log(tigerli._name); // undefined <-- von aussen kann nicht auf die priva
 
 
 ////////////////////////////////////////////////////////////////////// Öffentliche Methoden
+// Hat Performance-Nachteile, da die priviligierten öffentlichen Methoden für jede Objektinstanz existieren
 // Klasse
 class Tier {
   constructor(name, alter) {
@@ -26,3 +27,27 @@ class Tier {
 const tigerli = new Tier('Tigerli', 18);
 console.log(tigerli._name); // undefined <-- von aussen kann nicht auf die private Eigenschaft zugegriffen werden
 console.log(tigerli.getName()); // Tigerli <-- Aufruf von öffentlicher Methode
+
+
+
+
+
+// Kapselung in IIFE die (mit Closure) Tier als Rückgabewert gibt
+const Tier = (function() {
+  function Tier(name, alter) {
+    var _name = name;
+    var _alter = alter;
+    this.getName = function() {
+      return _name;
+    }
+  }
+  return Tier;
+})();
+const tigerli = new Tier('Tigerli', 3);
+tigerli._name = "Toger";
+console.log(tigerli.getName()); // Tigerli
+
+
+
+
+
