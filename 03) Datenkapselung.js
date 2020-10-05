6cfb0a1a58a733da317b62872812ffdb3cb10279
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-05 21:43:54 +0200

    Update 03) Datenkapselung.js

--- 03) Datenkapselung.js
+++ 03) Datenkapselung.js
@@ -1,9 +1,9 @@
 // Private Eigenschaften mit _ kennzeichnen --> let _name = 'peter';
-class Tiere {
+class Tier {
   constructor(name, alter) {
     let _name = name;
     let _alter = alter;
   }
 }
-const tigerli = new Tiere('Tigerli', 18);
+const tigerli = new Tier('Tigerli', 18);
 console.log(tigerli.name); // undefined <-- von aussen kann nicht auf die private Eigenschaft zugegriffen werden
