f3d2fec53c35f5104ebdbfa3eba7acf831af9c8b
IT-Sammlung <66636732+IT-Sammlung@users.noreply.github.com>
2020-10-08 17:29:31 +0200

    Update 01) array.js

--- 01) array.js
+++ 01) array.js
@@ -5,3 +5,14 @@ const result = array1
 .filter(x => !isNaN(x)) // ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 .reduce((x, y) => x + y); // 1+2+3+4+5+6+7+8+9 = 45
 console.log(result); // 45
+
+
+
+
+
+// Entferne duplizierte Einträge aus Array
+const nums = [0,1,2,3,4,5,6,7,8,9,9];
+function removeDuplicate(nums) {
+  return [...new Set(nums)];
+}
+removeDuplicate(nums);
